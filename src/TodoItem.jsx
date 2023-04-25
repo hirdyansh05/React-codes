97e826c5c7e7e83f6e89ef0370556899bcac8b0b
HIRDYANSH SHARMA <72224878+hirdyansh05@users.noreply.github.com>
2023-04-25 21:50:32 +0530

    adding codes

---
+++ src/TodoItem.jsx
@@ -0,0 +1,17 @@
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodos }) {
+  return (
+    <li>
+      <label htmlFor="">
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodos(id)} className="btn btn-danger">
+        Delete
+      </button>
+    </li>
+  );
+}
